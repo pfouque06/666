de85ed3b6130cf0f4b3a7fc9e1abeb6caaf17aee
pfouque06 <pfouque@gmail.com>
2020-02-04 03:22:25 +0100

    b0.24.01 -Dialog Menu WIP

---
+++ Menu.java
@@ -0,0 +1,147 @@
+package _666_;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JSpinner;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.JCheckBox;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class Menu extends JDialog {
+	
+	private MenuInfo info = new MenuInfo();
+	private boolean sendData;
+	private JPanel depositBorder, warningBorder, limiteBorder, gainBorder, phaseBorder, tourBorder, betBorder;
+	private JSpinner depositSpinner, warningSpinner, LimiteSpinner, gainSpinner, phaseSpinner, tourSpinner, betSpinner;
+	private JCheckBox chckbxSimulation, chckbxAutoSpin;
+	private JButton btnOk;
+	
+	public Menu(JFrame parent, String title, boolean modal){
+	    super(parent, title, modal);
+	    //this.setSize(310, 350); ??
+	    this.setLocationRelativeTo(null);
+	    this.setResizable(false);
+	    this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+	    this.buildContent();
+	}
+	
+	public MenuInfo showMenuInfo(){
+	    this.sendData = false;
+	    this.setVisible(true);      
+	    return this.info;      
+	  }
+	
+	public void buildContent() {
+		this.getContentPane().setLayout(null);
+		
+		depositBorder = new JPanel();
+		depositBorder.setLayout(null);
+		depositBorder.setBorder(new TitledBorder(null, "Deposit", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		depositBorder.setBounds(12, 12, 147, 52);
+		this.getContentPane().add(depositBorder);
+		
+		depositSpinner = new JSpinner();
+		depositSpinner.setBounds(5, 17, 130, 30);
+		depositBorder.add(depositSpinner);
+		
+		warningBorder = new JPanel();
+		warningBorder.setLayout(null);
+		warningBorder.setBorder(new TitledBorder(null, "Warning", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		warningBorder.setBounds(12, 76, 147, 52);
+		this.getContentPane().add(warningBorder);
+		
+		warningSpinner = new JSpinner();
+		warningSpinner.setBounds(5, 17, 130, 30);
+		warningBorder.add(warningSpinner);
+		
+		limiteBorder = new JPanel();
+		limiteBorder.setLayout(null);
+		limiteBorder.setBorder(new TitledBorder(null, "Limite", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		limiteBorder.setBounds(12, 140, 147, 52);
+		this.getContentPane().add(limiteBorder);
+		
+		LimiteSpinner = new JSpinner();
+		LimiteSpinner.setBounds(5, 17, 130, 30);
+		limiteBorder.add(LimiteSpinner);
+		
+		gainBorder = new JPanel();
+		gainBorder.setLayout(null);
+		gainBorder.setBorder(new TitledBorder(null, "Gain", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		gainBorder.setBounds(12, 204, 147, 52);
+		this.getContentPane().add(gainBorder);
+		
+		gainSpinner = new JSpinner();
+		gainSpinner.setBounds(5, 17, 130, 30);
+		gainBorder.add(gainSpinner);
+
+		phaseBorder = new JPanel();
+		phaseBorder.setLayout(null);
+		phaseBorder.setBorder(new TitledBorder(null, "phase", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		phaseBorder.setBounds(197, 12, 95, 52);
+		this.getContentPane().add(phaseBorder);
+		
+		phaseSpinner = new JSpinner();
+		phaseSpinner.setBounds(5, 17, 80, 30);
+		phaseBorder.add(phaseSpinner);
+		
+		tourBorder = new JPanel();
+		tourBorder.setLayout(null);
+		tourBorder.setBorder(new TitledBorder(null, "Tour", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		tourBorder.setBounds(197, 76, 95, 52);
+		this.getContentPane().add(tourBorder);
+		
+		tourSpinner = new JSpinner();
+		tourSpinner.setBounds(5, 17, 80, 30);
+		tourBorder.add(tourSpinner);
+		
+		betBorder = new JPanel();
+		betBorder.setLayout(null);
+		betBorder.setBorder(new TitledBorder(null, "Bet", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		betBorder.setBounds(197, 140, 95, 52);
+		this.getContentPane().add(betBorder);
+		
+		betSpinner = new JSpinner();
+		betSpinner.setBounds(5, 17, 80, 30);
+		betBorder.add(betSpinner);
+		
+
+		chckbxSimulation = new JCheckBox("Simulation");
+		chckbxSimulation.setBounds(181, 202, 111, 23);
+		this.getContentPane().add(chckbxSimulation);
+		
+		chckbxAutoSpin = new JCheckBox("Auto Spin");
+		chckbxAutoSpin.setBounds(181, 231, 111, 23);
+		this.getContentPane().add(chckbxAutoSpin);
+		
+		btnOk = new JButton("OK");
+		btnOk.setBounds(232, 293, 60, 25);
+		this.getContentPane().add(btnOk);
+		
+		btnOk.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				info = new MenuInfo();
+				info.setDeposit(LimiteSpinner.getValue().toString());
+				info.setLimite(LimiteSpinner.getValue().toString());
+				info.setWarning(warningSpinner.getValue().toString());
+				info.setGain(gainSpinner.getValue().toString());
+				info.setPhase(phaseSpinner.getValue().toString());
+				info.setTour(tourSpinner.getValue().toString());
+				info.setBet(betSpinner.getValue().toString());
+				info.setSimMode(chckbxSimulation.isSelected());
+				info.setAutoMode(chckbxAutoSpin.isSelected());
+				
+		        setVisible(false);
+				
+			}
+		});
+		
+	}
+}
---
+++ MenuInfo.java
@@ -0,0 +1,167 @@
+package _666_;
+
+public class MenuInfo {
+
+	private String deposit, limite, warning, gain;
+	private String phase, tour, bet;
+	private boolean simMode, autoMode;
+
+	public MenuInfo() {
+		super();
+	}
+
+	public MenuInfo(String deposit, String limite, String warning, String gain, String phase, String tour, String bet,
+			boolean simMode, boolean autoMode) {
+		super();
+		this.deposit = deposit;
+		this.limite = limite;
+		this.warning = warning;
+		this.gain = gain;
+		this.phase = phase;
+		this.tour = tour;
+		this.bet = bet;
+		this.simMode = simMode;
+		this.autoMode = autoMode;
+	}
+
+	/**
+	 * @return the deposit
+	 */
+	public String getDeposit() {
+		return deposit;
+	}
+
+	/**
+	 * @param deposit the deposit to set
+	 */
+	public void setDeposit(String deposit) {
+		this.deposit = deposit;
+	}
+
+	/**
+	 * @return the limite
+	 */
+	public String getLimite() {
+		return limite;
+	}
+
+	/**
+	 * @param limite the limite to set
+	 */
+	public void setLimite(String limite) {
+		this.limite = limite;
+	}
+
+	/**
+	 * @return the warning
+	 */
+	public String getWarning() {
+		return warning;
+	}
+
+	/**
+	 * @param warning the warning to set
+	 */
+	public void setWarning(String warning) {
+		this.warning = warning;
+	}
+
+	/**
+	 * @return the gain
+	 */
+	public String getGain() {
+		return gain;
+	}
+
+	/**
+	 * @param gain the gain to set
+	 */
+	public void setGain(String gain) {
+		this.gain = gain;
+	}
+
+	/**
+	 * @return the phase
+	 */
+	public String getPhase() {
+		return phase;
+	}
+
+	/**
+	 * @param phase the phase to set
+	 */
+	public void setPhase(String phase) {
+		this.phase = phase;
+	}
+
+	/**
+	 * @return the tour
+	 */
+	public String getTour() {
+		return tour;
+	}
+
+	/**
+	 * @param tour the tour to set
+	 */
+	public void setTour(String tour) {
+		this.tour = tour;
+	}
+
+	/**
+	 * @return the bet
+	 */
+	public String getBet() {
+		return bet;
+	}
+
+	/**
+	 * @param bet the bet to set
+	 */
+	public void setBet(String bet) {
+		this.bet = bet;
+	}
+
+	/**
+	 * @return the simMode
+	 */
+	public boolean isSimMode() {
+		return simMode;
+	}
+
+	/**
+	 * @param simMode the simMode to set
+	 */
+	public void setSimMode(boolean simMode) {
+		this.simMode = simMode;
+	}
+
+	/**
+	 * @return the autoMode
+	 */
+	public boolean isAutoMode() {
+		return autoMode;
+	}
+
+	/**
+	 * @param autoMode the autoMode to set
+	 */
+	public void setAutoMode(boolean autoMode) {
+		this.autoMode = autoMode;
+	}
+
+	@Override
+	public String toString() {
+		return "MenuInfo :" + 
+				"\ndeposit=" + deposit + 
+				"\nlimite=" + limite + 
+				"\nwarning=" + warning + 
+				"\ngain=" + gain
+				+ "\nphase=" + phase + 
+				"\ntour=" + tour + 
+				"\nbet=" + bet + 
+				"\nsimMode=" + simMode + 
+				"\nautoMode=" + autoMode;
+	}
+
+}
